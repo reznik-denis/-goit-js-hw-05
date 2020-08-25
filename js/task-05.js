876ecebdc85663cda932894abcd5c29b4db03a5a
Reznik Denis <63472391+reznik-denis@users.noreply.github.com>
2020-08-25 18:22:15 +0300

    добавлет решение 5 задачки

--- js/task-05.js
+++ js/task-05.js
@@ -1,50 +1,177 @@
-// bind для замены this в методах объекта
-// Оформи вызов метода invokeInventoryAction таким образом, чтобы в качестве this для методов
+// класс
+// Напиши класс Car с указанными свойствами и методами.
 
-// inventory.add
-// inventory.remove выступал объект inventory
+// class Car {
+//     /*
+//      * Добавь `статический` метод 
+//      * `getSpecs(car)`, который принимает 
+//      * объект-машину как параметр 
+//      * и возвращает шаблонную строку 
+//      * со свойствами и значениями объекта.
+//      * Свойства: 
+//      *   maxSpeed, 
+//      *   speed, 
+//      *   isOn, 
+//      *   distance,
+//      *   price
+//      * Пример строки, полученной этим методом:
+//      * 'maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000'
+//      */
 
-const inventory = {
-    items: ['Knife', 'Gas mask'],
-    add(itemName) {
-        this.items.push(itemName);
-        return `Adding ${itemName} to inventory`;
-    },
-    remove(itemName) {
-        this.items = this.items.filter(item => item !== itemName);
-        return `Removing ${itemName} from inventory`;
-    },
-};
+//     /*
+//      * Конструктор получает объект настроек.
+//      *
+//      * Добавь свойства будущеего экземпляра класса:
+//      *  speed - текущая скорость, 
+//      *          начальное значение `0`
+//      *  price - цена автомобиля
+//      *  maxSpeed - максимальная скорость
+//      *  isOn - заведен ли автомобиль. 
+//      *         Значения `true` или `false`,  
+//      *         начальное значение false
+//      *  distance - пробег в километрах, 
+//      *             начальное значение `0`
+//      */
+//     constructor() { }
 
-const invokeInventoryAction = function (itemName, action) {
-    const act = action(itemName);
-    const msg = `Invoking action on ${itemName}`;
-    return { act, msg };
-};
+//     /*
+//      * Добавь геттер и сеттер 
+//      * для свойства `price`, который будет 
+//      * работать с свойством цены автомобиля.
+//      * 
+//      * ВАЖНО: для записи методов get и set
+//      * значение параметра записывают с 
+//      * подчеркиванием. См. ниже пример 1.
+//      */
 
-const invokeAdd = invokeInventoryAction(
-    'Medkit',
-    inventory.add.bind(inventory),  // Write code in this line
-);
-const arrayAdd = [...inventory.items];
+//     /*
+//      * Метод, который заводит автомобиль
+//      * Записывает в свойство `isOn` значение `true`
+//      */
+//     turnOn() { }
 
-console.log(invokeAdd);
-//{ act: 'Adding Medkit to inventory', msg: 'Invoking action on Medkit' }
+//     /*
+//      * Метод, чтобы заглушить автомобиль
+//      * Этот метод должен записывать 
+//      * в свойство isOn значение false,
+//      * и сбрасывать текущую скорость до 0
+//      */
+//     turnOff() { }
 
-console.log(arrayAdd);
-// ['Knife', 'Gas mask', 'Medkit']
+//     /*
+//      * Этот метод должен добавлять 
+//      * к свойству `speed` полученное
+//      * значение, при условии, 
+//      * что результирующая скорость
+//      * не больше чем значение свойства `maxSpeed`
+//      */
+//     accelerate(value) { }
 
+//     /*
+//      * Этот метод должен отнимать 
+//      * от свойства `speed` 
+//      * полученное значение, при условии, 
+//      * что результирующая скорость не меньше 0
+//      */
 
-const invokeRemove = invokeInventoryAction(
-    'Gas mask',
-    inventory.remove.bind(inventory),  // Write code in this line
-);
+//     decelerate(value) { }
 
-const arrayRemove = [...inventory.items];
+//     /*
+//      * Этот метод должен добавлять к свойству 
+//      * `distance` пробег в километрах, 
+//      * т.е. hours * speed,
+//      * но только в том случае, 
+//      * если машина заведена!
+//      */
+//     drive(hours) { }
+// }
 
-console.log(invokeRemove);
-//{ act: 'Removing Gas mask from inventory', msg: 'Invoking action on Gas mask' }
+// #### Пример 1.
 
-console.log(arrayRemove);
-// ['Knife', 'Medkit']
+//     ```js
+// class Guest {
+//   // Собственные свойства класса размещаем в конструкторе
+//   constructor(name, roomNumber) {
+//     /* параметр name не должен 
+//      *   совпадать с названием свойства
+//      *   но должен быть похож.
+//      *   Поэтому используют прием смены
+//      *   названия добавляя символ '_'.
+//      *   Хотя это может любой другой символ
+//      */
+//     this._name = name;
+//   }
 
+//   // Используем геттеры и сеттеры для описания интерфейса доступа к свойствам
+//   get name() {
+//     return this._name;
+//   }
+
+//   set name(value) {
+//     this._name = value;
+//   }
+// }
+
+class Car {
+    // Write code under this line
+    static getSpecs(car) { 
+        return `maxSpeed: ${car.maxSpeed}, speed: ${car.speed}, isOn: ${car.isOn}, distance: ${car.distance}, price: ${car.price}`;  
+    }
+    constructor({ speed = 0, price, maxSpeed, isOn = false, distance = 0 }) { 
+        this.speed = speed;
+        this._price = price;
+        this.maxSpeed = maxSpeed;
+        this.isOn = isOn;
+        this.distance = distance;
+    }
+    get price() { 
+        return this._price;
+    }
+    set price(value) {
+        return this._price = value;
+    }
+    turnOn() { 
+        return this.isOn = true;
+    }
+    turnOff() { 
+        this.isOn = false; this.speed = 0;
+    }
+    accelerate(value) { 
+        if (this.maxSpeed >= (this.speed + value)) {
+            return this.speed += value;
+        } else {
+            return this.speed = this.maxSpeed;
+        }
+    }
+    decelerate(value) { 
+        if ((this.speed - value) >= 0) {
+            return this.speed -= value;
+        } else {
+            return this.speed = 0;
+        }
+    }
+    drive(hours) { 
+        if (this.isOn) {
+            return this.distance += (hours * this.speed);
+        }
+    }
+}
+
+const mustang = new Car({ maxSpeed: 200, price: 2000 });
+mustang.turnOn();
+mustang.accelerate(50);
+mustang.drive(2);
+
+console.log(Car.getSpecs(mustang));
+// 'maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000'
+
+mustang.decelerate(20);
+mustang.drive(1);
+mustang.turnOff();
+
+console.log(Car.getSpecs(mustang));
+// 'maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000'
+
+console.log(mustang.price); // 2000
+mustang.price = 4000;
+console.log(mustang.price); // 4000
