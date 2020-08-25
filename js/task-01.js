93a35dc35f299e65f587bcf1076dd849f539e1c1
Reznik Denis <63472391+reznik-denis@users.noreply.github.com>
2020-08-25 16:45:04 +0300

    добавляет решение задачки 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,32 +1,28 @@
-// Callback функция
-// Функция mapArray(array, cb), принимает 1 - м параметром array - массив чисел, а вторым параметром cb - функцию обратного вызова(callback).Функция mapArray создает новый массив numbers и заполняет его числами из массива array преобразованными функцией cb.
+// function-constructor
+// Напиши функцию - конструктор Account, которая создает объект со свойствами login и email.
 
-// Оформи создание массива numbers нужной длины используя new Array() и необходимый аргумент для задания длины, равной длине array.
-
-// Напиши функцию обратного вызова addIndex, которая принимает два параметра - element и index и возвращает число - сумму element и index(сложение).
-
-// Напиши функцию обратного вызова subIndex, которая принимает два параметра - element и index и возвращает число - разность element и index(вычитание).
+// В prototype функции - конструктора добавь метод getInfo(), который возвращает строку со значениями свойств login и email объекта.
 
 // Write code under this line
-const addIndex = (element, index) => element + index;
-// Write code under this line
-const subIndex = (element, index) => element - index;
-
-function mapArray(array, cb) {
-    'use strict';
-    // Write code under this line
-    const numbers = new Array(array.length);
-    for (let i = 0; i < array.length; i += 1) {
-        const element = array[i];
-        const index = i;
-        numbers[i] = cb(element, index);
-    }
-    return numbers;
+const Account = function (login, email) {
+    this.login = login;
+    this.email = email;
 }
 
-const arr = [1, 2, 3, 4, 5];
+Account.prototype.getInfo = function () {
+    return `login : ${this.login}, email: ${this.email}`;
+}
+    
 
-console.log(mapArray(arr, addIndex));
-// [1, 3, 5, 7, 9]
-console.log(mapArray(arr, subIndex));
-// [1, 1, 1, 1, 1]
+console.log(typeof Account.prototype.getInfo);
+// 'function'
+
+const mango = new Account('Mangozedog', 'mango@dog.woof');
+console.log(mango.getInfo());
+// 'login : Mangozedog, email: mango@dog.woof'
+
+const poly = new Account('Poly', 'poly@mail.com');
+console.log(poly.getInfo());
+// 'login : Poly, email: poly@mail.com'
+
+console.log(mango)
